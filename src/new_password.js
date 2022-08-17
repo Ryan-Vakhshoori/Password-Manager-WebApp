a7ec9e3042db9c69f35cf8a53c2f72d9c6c45fcd
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-16 22:11:53 -0700

    added new_password.js

---
+++ src/new_password.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Popup as ReactPopup } from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { useState } from "react";
+
+const Popup = () => {
+  const [site, setSite] = useState("");
+  const [username, setUsername] = useState("");
+  return (
+    <ReactPopup
+      trigger={<button>add new password</button>}
+      modal
+    >
+      <form>
+        <label>
+          Site
+          <input
+            type="text"
+            value={site}
+            onChange={(e) => setSite(e.target.value)}
+          />
+        </label>
+        <label>
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+      </form>
+    </ReactPopup>
+  );
+};
+
+export default Popup;
