6f381b022a75e5a63e5dc128f8555e288ea6d822
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-22 20:31:15 -0700

    added folders

---
+++ src/pages/passwords.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Modal from "../components/Modal";
+import Table from "../components/Table";
+
+function Passwords() {
+  return (
+    <div>
+      <Modal />
+      <Table />
+    </div>
+  );
+}
+
+export default Passwords;
