231ad8c283f07216e7e905da994470165c465fbb
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-23 10:47:37 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDO7ZjQJ1BseZWIoAQQJsmri7JHi1xIPVM",
+  authDomain: "password-manager-a044d.firebaseapp.com",
+  projectId: "password-manager-a044d",
+  storageBucket: "password-manager-a044d.appspot.com",
+  messagingSenderId: "169481468260",
+  appId: "1:169481468260:web:deefb62e5cb55de798e271",
+  measurementId: "G-H9B1X0TR7T"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
