ce05037066f660990d58513b603dbe1d7e57f73d
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-24 16:42:26 -0700

    created routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+// axios.get("http://localhost:3001").then(function (response) {
+//   console.log("successfully added user");
+// }).catch(function (error) {
+//   console.log(error);
+// });
 
 function App() {
   return (
