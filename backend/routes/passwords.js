7ef3401e150f4059fe0e1ae403a39b1ea314f90b
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-25 14:54:07 -0700

    finally

--- backend/routes/passwords.js
+++ backend/routes/passwords.js
@@ -1,25 +1,23 @@
 import express from "express";
-import { collection, doc, updateDoc } from "firebase/firestore";
+import { doc, setDoc, updateDoc } from "firebase/firestore";
 import db from "../firestore.js";
 
 const passwordsRouter = express();
 
 passwordsRouter.post("/new-password", async (req, res) => {
   try {
-    await db.collection("users").doc(req.body.docID).set(
-    {
-      site: req.body.site,
-      username: req.body.username,
-      password: req.body.password,
-    },
-      { merge: true },
+    const docRef = doc(db, "users", req.body.docID);
+    await setDoc(
+      docRef,
+      {
+        [req.body.site]: {
+          site: req.body.site,
+          username: req.body.username,
+          password: req.body.password,
+        },
+      },
+      { merge: true }
     );
-    // const docRef = doc(db, "users", req.body.docID);
-    // await updateDoc(docRef, {
-    //   site: req.body.site,
-    //   username: req.body.username,
-    //   password: req.body.password,
-    // });
     res.status(200).send({ message: "success" });
   } catch (error) {
     res.status(500).send(error);
