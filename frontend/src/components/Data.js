f12daf43b1d73f40506a3c131d1429cbfaceb4fa
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2022-08-26 16:43:09 -0700

    ordered entries

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -13,7 +13,7 @@ function Data(props) {
         },
       })
       .then(function (response) {
-        setPasswords(response.data);
+        setPasswords(order(response.data));
         setLoading(false);
       })
       .catch(function (error) {
@@ -21,15 +21,26 @@ function Data(props) {
       });
   }, [props.docID]);
 
+  function order(arr) {
+    const orderedArr = [];
+    arr.forEach((element) => {
+      const temp = {
+        "site": element["site"],
+        "username": element["username"],
+        "password": element["password"],
+      };
+      orderedArr.push(temp);
+    });
+    return orderedArr;
+  }
+
   if (isLoading) {
     return <div>loading...</div>;
   }
 
   return (
     <div>
-      <pre>
-        {JSON.stringify(passwords, null, 2)}
-      </pre>
+      <pre>{JSON.stringify(passwords, null, 2)}</pre>
     </div>
   );
 }
--- frontend/src/pages/new-password.js
+++ frontend/src/pages/new-password.js
@@ -1,5 +1,3 @@
-import ReactModal from "react-modal";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -31,50 +29,30 @@ function NewPassword() {
       })
       .then(function (response) {
         navigate(-1);
-
       })
       .catch(function (error) {
         console.log(error);
       });
   };
-  // const [modalIsOpen, setIsOpen] = useState(false);
-
-  // function openModal() {
-  //   setIsOpen(true);
-  // }
-
-  // function closeModal() {
-  //   setIsOpen(false);
-  // }
 
   return (
-    // <div>
-    //   <button onClick={openModal}>add new password</button>
-    //   <ReactModal
-    //     isOpen={modalIsOpen}
-    //     onRequestClose={closeModal}
-    //     ariaHideApp={false}
-    //   >
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            site
-            <input type="text" {...register("site")} />
-          </label>
-          <br />
-          <label>
-            username
-            <input type="text" {...register("username")} />
-          </label>
-          <br />
-          <label>
-            password
-            <input type="text" {...register("password")} />
-          </label>
-          <input type="submit" />
-        </form>
-    //     <button onClick={closeModal}>cancel</button>
-    //   </ReactModal>
-    // </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        site
+        <input type="text" {...register("site")} />
+      </label>
+      <br />
+      <label>
+        username
+        <input type="text" {...register("username")} />
+      </label>
+      <br />
+      <label>
+        password
+        <input type="text" {...register("password")} />
+      </label>
+      <input type="submit" />
+    </form>
   );
 }
 
